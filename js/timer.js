9d67e460fcf95b54308b870619574f91cfcfd72d
Diana <Shimlykhdiana@gmail.com>
2025-11-10 20:42:14 +0300

    feature: functional timer

---
+++ js/timer.js
@@ -0,0 +1,96 @@
+const daysBlock = document.querySelector(".timer__days");
+const hoursBlock = document.querySelector(".timer__hours");
+const minutesBlock = document.querySelector(".timer__minutes");
+const secondsBlock = document.querySelector(".timer__seconds");
+const timerBlock = document.querySelectorAll(".timer__count");
+
+let interval;
+
+const numWord = (value, words) => {
+  value = Math.abs(value) % 100; //избавляемся от отрицательных значений
+
+  const lastNum = value % 10;
+  if (value == 0) return words[2];
+  if (value > 10 && value < 20) return words[2];
+  if (lastNum > 1 && lastNum < 5) return words[1];
+  if (lastNum === 1) return words[0];
+  return words[2];
+};
+
+const updateTimer = () => {
+  const date = new Date();
+  const dateDeadline = new Date("13 november 2025").getTime();
+  const timeRemaining = (dateDeadline - date) / 1000;
+
+  const days = Math.floor(timeRemaining / 60 / 60 / 24);
+  const hours = Math.floor((timeRemaining / 60 / 60) % 24);
+  const minutes = Math.floor((timeRemaining / 60) % 60);
+  const seconds = Math.floor(timeRemaining % 60);
+
+  const fDays = days < 10 ? "0" + days : days;
+  const fHours = hours < 10 ? "0" + hours : hours;
+  const fMinutes = minutes < 10 ? "0" + minutes : minutes;
+  const fSeconds = seconds < 10 ? "0" + seconds : seconds;
+
+  daysBlock.textContent = fDays;
+  hoursBlock.textContent = fHours;
+  minutesBlock.textContent = fMinutes;
+  secondsBlock.textContent = fSeconds;
+
+  secondsBlock.nextElementSibling.textContent = numWord(seconds, [
+    "секунда",
+    "секунды",
+    " секунд",
+  ]);
+  minutesBlock.nextElementSibling.textContent = numWord(minutes, [
+    "минута",
+    "минуты",
+    "минут",
+  ]);
+  hoursBlock.nextElementSibling.textContent = numWord(hours, [
+    "час",
+    "часа",
+    "часов",
+  ]);
+  daysBlock.nextElementSibling.textContent = numWord(days, [
+    "день",
+    "дня",
+    "дней",
+  ]);
+
+  if (timeRemaining <= 0) {
+    clearInterval(interval);
+    daysBlock.textContent = "00";
+    hoursBlock.textContent = "00";
+    minutesBlock.textContent = "00";
+    secondsBlock.textContent = "00";
+
+    daysBlock.nextElementSibling.textContent = numWord(0, [
+      "день",
+      "дня",
+      "дней",
+    ]);
+    hoursBlock.nextElementSibling.textContent = numWord(0, [
+      "час",
+      "часа",
+      "часов",
+    ]);
+    minutesBlock.nextElementSibling.textContent = numWord(0, [
+      "минута",
+      "минуты",
+      "минут",
+    ]);
+    secondsBlock.nextElementSibling.textContent = numWord(0, [
+      "секунда",
+      "секунды",
+      "секунд",
+    ]);
+
+    timerBlock.forEach((item) => {
+      item.style.color = "red";
+    });
+  }
+};
+
+updateTimer();
+interval = setInterval(updateTimer, 500); //1000 мс вызывает неестественную задержку, лучше обновлять чаще
